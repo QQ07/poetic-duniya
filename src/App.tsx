cc1743106dba894305309b251c23ef72f84fcc09
Rohan Vaidya <rohanv.rvaidya@gmail.com>
2024-09-08 01:21:21 +0530

    before update

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,24 @@ import { Blog } from "./pages/Blog";
 import { Test } from "./pages/Test";
 import Navbar from "./components/Navbar";
 import Create from "./pages/Create";
+import { RecoilRoot } from "recoil";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-      <Navbar/>
-        <Routes>
-        
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/blog/:id" element={<Blog />} />
-          <Route path="/blogs" element={<Blogs />} />
-          <Route path="/create" element={<Create />} />
-          <Route path="/test" element={<Test />} />
-        </Routes>
-      </BrowserRouter>
+      <RecoilRoot>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/signin" element={<Signin />} />
+            <Route path="/blog/:id" element={<Blog />} />
+            <Route path="/blogs" element={<Blogs />} />
+            <Route path="/create" element={<Create />} />
+            <Route path="/test" element={<Test />} />
+          </Routes>
+        </BrowserRouter>
+      </RecoilRoot>
     </>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,26 @@
 import { Link } from "react-router-dom";
 import { Avatar } from "./BlogCard";
+import { useRecoilState } from "recoil";
+import { userState } from "../store/atoms";
 
 export default function Navbar() {
+  const [user, setUser] = useRecoilState(userState);
+  console.log("reocial: " + user.name);
   // const navigate = useNavigate();
   return (
     <div className="border-b flex justify-between px-10 py-5 border-2 items-center">
-      <div className="text-3xl">Bloggle</div>
+      <div className="text-3xl">Poetic Duniya</div>
       <div className="flex gap-3">
         {/* <Avatar Name={"authorName"} size={11} /> */}
         <Link to={`/create`}>
           <button
             type="button"
-           className="text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center  "
+            className="text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-1.5 text-center  "
           >
-            Create
+            New
           </button>
         </Link>
-        <Avatar Name="Rohan Vaidya" size={20} />
+        {user.name ? <Avatar Name={user.name} size={20} /> : <></>}
       </div>
     </div>
   );
